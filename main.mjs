a02d3cb2ee70d22d8f43526ba6cdb214d4c27a1f
Don-Isdale <Don.Isdale@gmail.com>
2025-07-27 19:10:56 +1000

    add fetchDotPlotData() for IPK / PanBARLEX
    main.mjs : add ipkPanbarlex / ipk-panbarlex.js
    package.json : increment patch version.
    add ipk-panbarlex.js, with fetchDotPlotData(),  getDotPlot(). The first version was partially generated by ChatGPT based on /dotplot network request recorded by Web Inspector : Copy as Fetch, modified to add .slice(0, 2), 2nd and change params genotype,contig,start,end to an array of objects, declare Interval;  added test data Self_Alignment, Pairwise_Alignment.

--- main.mjs
+++ main.mjs
@@ -1,4 +1,8 @@
+//------------------------------------------------------------------------------
+
 import * as vcfFeature from './src/utils/data/vcf-feature.js';
+
+//------------------------------------------------------------------------------
 
 /* These functions won't be used in node.js server ATM,
  * They are very close to the synonymous files in src/utils/data/
@@ -8,12 +12,16 @@ import * as vcfFeature from './src/utils/data/vcf-feature.js';
  *  src/utils/data/nodeJs/germinate.js
  */
 
+//------------------------------------------------------------------------------
+
 import * as genolinkPassport  from './src/utils/data/genolink-passport.js';
+import * as ipkPanbarlex      from './src/utils/data/ipk-panbarlex.js';
 import * as brapiGenotype     from './src/utils/data/brapi-genotype.js';
 import * as germinateGenotype from './src/utils/data/germinate-genotype.js';
 import * as germinate         from './src/utils/data/germinate.js';
 /* Alternately use either browser : bluebird, or node : util */
 import * as promisify         from './src/utils/data/promisify.js';
 
-export default {vcfFeature, genolinkPassport, brapiGenotype, germinateGenotype, germinate, promisify};
+export default {vcfFeature, genolinkPassport, ipkPanbarlex, brapiGenotype, germinateGenotype, germinate, promisify};
 
+//------------------------------------------------------------------------------
